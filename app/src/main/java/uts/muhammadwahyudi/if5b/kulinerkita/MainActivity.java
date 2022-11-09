02f50703b4cb68d7a255170ed59d65be52a9ac56
NathaciaLais <nathacialais@gmail.com>
2022-11-09 16:45:07 +0700

    Navigati top hilang

--- app/src/main/java/uts/muhammadwahyudi/if5b/kulinerkita/MainActivity.java
+++ app/src/main/java/uts/muhammadwahyudi/if5b/kulinerkita/MainActivity.java
@@ -1,6 +1,9 @@
 package uts.muhammadwahyudi.if5b.kulinerkita;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void bukaFragment(Fragment FRG){
+        FragmentManager FM = getSupportFragmentManager();
+        FragmentTransaction FM = FM.beginTranscation();
     }
 
     public boolean onCreateOptionMenu(Menu menu) {
